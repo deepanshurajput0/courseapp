8694e0cb8b0005bfb458395a33d1dddbc6d5e6ce
deepanshurajput0 <140803342+deepanshurajput0@users.noreply.github.com>
2023-08-04 00:12:18 -0700

    Update sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,7 +3,7 @@ export const sendToken =(res,user,message,statuscode=200)=>{
     const options = {
         expires: new Date(Date.now()+ 15*24*60*60*1000),
         httpOnly:true,
-        // secure:true,
+        secure:true,
         samSite:"none", 
 
     };
