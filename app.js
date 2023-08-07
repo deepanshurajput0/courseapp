23e5f9f28bd729248bfd2349c4d354a61f1f570b
deepanshurajput0 <140803342+deepanshurajput0@users.noreply.github.com>
2023-08-07 10:09:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 
 app.use(cookieParser())
 app.use(cors({
-    origin: true ,
+    origin: process.env.FRONTEND_URL,
     credentials: true, 
     methods: ["GET","POST","PUT","DELETE"]
 }))
